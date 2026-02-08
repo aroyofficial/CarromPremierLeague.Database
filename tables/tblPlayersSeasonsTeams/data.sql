5fde1db0e87c1302b2fb85b1b2c510869ca4df1d
aroyofficial <aroy02072000@gmail.com>
2026-02-08 17:49:51 +0530

    Register new team members for season 2

--- tables/tblPlayersSeasonsTeams/data.sql
+++ tables/tblPlayersSeasonsTeams/data.sql
@@ -1,14 +1,23 @@
-INSERT INTO tblPlayersSeasonsTeams (Id, PlayerId, SeasonId, TeamId)
+INSERT INTO tblPlayersSeasonsTeams (Id, PlayerId, SeasonId, TeamId, Void)
 VALUES
-    (1, 1, 1, 1),
-    (2, 2, 1, 1),
-    (3, 3, 1, 4),
-    (4, 9, 1, 4),
-    (5, 5, 1, 2),
-    (6, 6, 1, 2),
-    (7, 4, 1, 3),
-    (8, 10, 1, 3)
+    (1, 1, 1, 1, 0),
+    (2, 2, 1, 1, 0),
+    (3, 3, 1, 4, 0),
+    (4, 9, 1, 4, 0),
+    (5, 5, 1, 2, 0),
+    (6, 6, 1, 2, 0),
+    (7, 4, 1, 3, 0),
+    (8, 10, 1, 3, 0),
+    (9, 6, 2, 1, 0),
+    (10, 7, 2, 1, 0),
+    (11, 11, 2, 2, 0),
+    (12, 3, 2, 2, 0),
+    (13, 4, 2, 3, 0),
+    (14, 1, 2, 3, 0),
+    (15, 5, 2, 4, 0),
+    (16, 2, 2, 4, 0)
 ON DUPLICATE KEY UPDATE
     PlayerId = VALUES(PlayerId),
     SeasonId = VALUES(SeasonId),
-    TeamId = VALUES(TeamId);
+    TeamId = VALUES(TeamId),
+    Void = VALUES(Void);
